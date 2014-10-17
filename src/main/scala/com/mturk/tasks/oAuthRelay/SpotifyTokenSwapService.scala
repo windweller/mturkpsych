806f2c0deae5ccf2980df6ecb4c1cbc9e7d8e2d9
windwller <leo.niecn@gmail.com>
2014-10-17 17:22:40 -0400

    Add Spotify OAuth support

--- src/main/scala/com/mturk/tasks/oAuthRelay/SpotifyTokenSwapService.scala
+++ src/main/scala/com/mturk/tasks/oAuthRelay/SpotifyTokenSwapService.scala
@@ -2,11 +2,16 @@ package com.mturk.tasks.oAuthRelay
 
 import akka.actor.{ActorSystem, ActorRef}
 import akka.util.Timeout
+import com.mturk.tasks.Util._
+import JsonImplicits._
+import org.json4s.JsonAST.JObject
 import spray.routing.Directives
 import SpotifyTokenSwapActorMsg._
 import akka.pattern.ask
 import spray.http.StatusCodes._
 import scala.concurrent.duration._
+import com.mturk.tasks.Util._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
 class SpotifyTokenSwapService (SpotifyTokenSwapActor: ActorRef)(implicit system: ActorSystem)
@@ -17,11 +22,22 @@ class SpotifyTokenSwapService (SpotifyTokenSwapActor: ActorRef)(implicit system:
   lazy val route =
     pathPrefix("oauthrelay") {
       pathPrefix("spotify") {
-        get {
-          val response = (SpotifyTokenSwapActor ? Authenticate)
-                      .mapTo[TransOk]
-                      .map(result => (OK, result.statusCode + ": " + result.text))
-          complete(response)
+        post {
+          entity(as[JObject]) { jObject =>
+
+            if (jObject.isDefined("code")) {
+              val response = (SpotifyTokenSwapActor ? Authenticate(jObject.getValue("code")))
+                .mapTo[TransOk]
+                .map(result => (OK, result.statusCode + ": " + result.text))
+              complete(response)
+            }else{
+              val response = (SpotifyTokenSwapActor ? Authenticate(""))
+                .mapTo[TransOk]
+                .map(result => (OK, result.statusCode + ": " + result.text))
+              complete(response)
+            }
+
+          }
         }
       }
     }
