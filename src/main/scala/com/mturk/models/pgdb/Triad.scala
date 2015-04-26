b53e7e298cd8be2989f7f9146677a713c30f4370
windweller <leo.niecn@gmail.com>
2015-04-25 21:31:47 -0400

    fixed

--- src/main/scala/com/mturk/models/pgdb/Triad.scala
+++ src/main/scala/com/mturk/models/pgdb/Triad.scala
@@ -6,10 +6,10 @@ import scala.util.Try
 object Triad {
 
 //  val fields = List("verbTop", "verbTopCate", "verbLeft", "verbLeftCate",
-//    "verbRight", "verbRightCate", "predict", "reactionTime", "response")
+//    "verbRight", "verbRightCate", "predict", "reactionTime", "response", "randomId")
 
   case class Triad(id: Option[Int], commToken: String, phase: String, verbTop: String, verbLeft: String,
-                       verbRight: String, reactionTime: String, response: String)
+                       verbRight: String, reactionTime: String, response: String, randomId: String)
 
   class TriadTable(tag: Tag) extends Table[Triad](tag, "Triad4") {
     def id = column[Option[Int]]("TRIAD_ID", O.PrimaryKey, O.AutoInc)
@@ -20,9 +20,10 @@ object Triad {
     def verbRight = column[String]("TRIAD_VERB_RIGHT")
     def reactionTime = column[String]("TRIAD_REACTION_TIME")
     def response = column[String]("TRIAD_RESPONSE")
+    def randomId = column[String]("TRIAD_EXPID")
 
     def * = (id, commToken, phase, verbTop, verbLeft,
-       verbRight, reactionTime, response) <> (Triad.tupled, Triad.unapply)
+       verbRight, reactionTime, response, randomId) <> (Triad.tupled, Triad.unapply)
   }
 
   val triads = TableQuery[TriadTable]
--- src/main/scala/com/mturk/tasks/triadTest/TriadTestActor.scala
+++ src/main/scala/com/mturk/tasks/triadTest/TriadTestActor.scala
@@ -14,13 +14,13 @@ class TriadTestActor extends Actor with ActorLogging {
   def receive = {
     case JObjectTriadResult(jObject) =>
       val fields = List("commToken", "phase", "verbTop", "verbLeft",
-        "verbRight", "reactionTime", "response")
+        "verbRight", "reactionTime", "response", "expid")
 
       extractJObjectEntity(fields, jObject) match {
         case Right(fieldsValue) =>
           DAL.db.withSession { implicit session =>
             val result = Triad.insert(Triad.Triad(None, fieldsValue.head, fieldsValue(1),
-              fieldsValue(2), fieldsValue(3), fieldsValue(4), fieldsValue(5), fieldsValue(6)))
+              fieldsValue(2), fieldsValue(3), fieldsValue(4), fieldsValue(5), fieldsValue(6), fieldsValue(7)))
             result match {
               case Success(t) => sender ! TransOk(Some(t), None)
               case Failure(ex) => sender ! TransOk(None, Some(ex.getMessage))
