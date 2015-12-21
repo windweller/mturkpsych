2e00fe77ecd482f58d283e41672522fe68d87c22
eleetboy <leetdh1028@gmail.com>
2015-12-21 16:06:21 -0500

    update

--- views/js/vendor/vis.js
+++ views/js/vendor/vis.js
@@ -8395,8 +8395,8 @@ return /******/ (function(modules) { // webpackBootstrap
             for(var j = 0; j < spiderContainer[i].connectedTo.length; j++) {
               if(spiderContainer[i].connectedTo.length > 1)
                 console.log("MULTIPLE");
-              ctx.lineWidth = 3;
-              ctx.strokeStyle = this.gridColor;
+              ctx.lineWidth = 1;
+              ctx.strokeStyle = '#000000';
               ctx.beginPath();
               ctx.moveTo(spiderContainer[i].screenProjection.x, spiderContainer[i].screenProjection.y);
               ctx.lineTo(spiderContainer[i].connectedTo[j].screenProjection.x, spiderContainer[i].connectedTo[j].screenProjection.y);
